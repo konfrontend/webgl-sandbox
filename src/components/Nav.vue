776b5a6b833c95714497a2513a90b30e269148e1
Konstantin Dolbilov <konfrontend@gmail.com>
2020-02-01 22:08:42 +0400

    vulnerability found in yarn.lock

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/panels">Panels</router-link>
+    <router-link to="/panels">Test</router-link>
   </div>
 </template>
 
